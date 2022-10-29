3a28005bc5f776ba68682a44090bbddbf9241699
Lau :D <lautaroretamar@gmail.com>
2022-10-29 15:45:14 -0300

    feat: scheduler module

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from scraper import SendMessageTo
+from scheduler import Wait, At
 
 
 if __name__ == "__main__":
 
-    SendMessageTo('name','message', "https://web.whatsapp.com/", 1)
+    #Wait(1)
+    #At(29/10/2022 15:40:00)
+
+    date = '29/10/2022 15:40:00'
+
+    SendMessageTo('Name', f'Message scheduled at {date}', "https://web.whatsapp.com/", 1)
+
+    At(date)
+    SendMessageTo('Name','The message is: it works :D', "https://web.whatsapp.com/", 1)
---
+++ scheduler.py
@@ -0,0 +1,24 @@
+import time
+from datetime import datetime
+
+
+def At(date):
+    
+    # dd/mm/YY H:M:S
+    dateTimeNowFormated = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    while dateTimeNowFormated < date:
+        time.sleep(20)
+
+        dateTimeNowFormated = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print(dateTimeNowFormated + '   waiting for -->   ' + date)
+
+    return
+
+    
+def Wait(mins):
+    time.sleep(mins*60)
+
+    return
+        
+
